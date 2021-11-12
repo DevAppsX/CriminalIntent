2a6be20b8588805ca0a44c44af2122a0423de365
MobDe <MobDevX.outlook.com>
2021-11-12 14:59:20 +0700

    Create data class Crime

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 
 class CrimeFragment : Fragment() {
+    private lateinit var crime: Crime
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        crime = Crime()
 
     }
 
