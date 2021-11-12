31b5db1cc07df13194ea53ec232a170cc700a17f
MobDe <MobDevX.outlook.com>
2021-11-12 13:45:15 +0700

    add View Binding

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.criminalintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val layout = binding.root
+        setContentView(layout)
 
     }
 }
