6a847ece2cf31975cc07adee05020a5cbaa0fe33
MobDe <MobDevX.outlook.com>
2021-12-16 10:11:16 +0700

    Create DB

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -3,14 +3,6 @@ package com.example.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel: ViewModel() {
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for(i in 1..100) {
-            val crime = Crime()
-            crime.title = "Crime №$i"
-            crime.isSolved = i%2==0
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
 }
