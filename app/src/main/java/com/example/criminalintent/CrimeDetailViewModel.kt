e93c7fda0a13bf9b8049d2eb0ecf343158cb1994
MobDe <MobDevX.outlook.com>
2021-12-16 12:39:23 +0700

    Add Details

---
+++ app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.criminalintent
+
+import androidx.lifecycle.*
+import androidx.lifecycle.Observer
+import java.util.*
+
+class CrimeDetailViewModel() : ViewModel() {
+
+    private val crimeRepository = CrimeRepository.get()
+    private val crimeIdLiveData = MutableLiveData<UUID>()
+
+    var crimeLiveData: LiveData<Crime?> =
+        Transformations.switchMap(crimeIdLiveData) { crimeId ->
+            crimeRepository.getCrime(crimeId)
+        }
+
+    fun loadCrime(crimeId: UUID) {
+        crimeIdLiveData.value = crimeId
+    }
+/*
+    fun saveCrime(crime: Crime) {
+        crimeRepository.updateCrime(crime)
+    }*/
+}
